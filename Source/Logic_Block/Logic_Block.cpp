666a3ccac276b0e15903193cb559061828881710
j3vito <tugame69@gmail.com>
2026-05-19 13:13:14 +0200

    [C++]Init_Lesson_19_05_

---
+++ Source/Logic_Block/Logic_Block.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Logic_Block.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Logic_Block, "Logic_Block" );
